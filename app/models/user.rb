e3f6139f549954c8568e8234244fa8872ac85ba5
Alex Guzman <aguzma05@nyit.edu>
2017-12-11 23:35:21 -0500

    fixed a bug with the feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,10 +31,13 @@ class User < ActiveRecord::Base
 
   def feed
 
-     ActiveRecord::Base.connection.exec_query("SELECT u.name, a.created_at, e.title, e.event_id FROM attendances a
+
+     ActiveRecord::Base.connection.exec_query("SELECT u.name, a.created_at, e.title, e.event_id FROM
+                          attendances a
                             join events e on e.event_id = a.event_id
-                            join relationships r on r.follower_id = #{self.id}
+                            join relationships r on r.followed_id = a.user_id
                             join users u on u.user_id = r.followed_id
+                            WHERE r.follower_id = #{self.id}
                              order by a.created_at DESC")
 
   end
